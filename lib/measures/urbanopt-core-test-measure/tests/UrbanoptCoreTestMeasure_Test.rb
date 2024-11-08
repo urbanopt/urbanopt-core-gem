2a436cd516eb988d185ff0ddb1d32a8084530b9e
kflemin <2205659+kflemin@users.noreply.github.com>
2024-11-08 10:16:15 -0700

    added test measure

---
+++ lib/measures/urbanopt-core-test-measure/tests/UrbanoptCoreTestMeasure_Test.rb
@@ -0,0 +1,83 @@
+# *******************************************************************************
+# OpenStudio(R), Copyright (c) Alliance for Sustainable Energy, LLC.
+# See also https://openstudio.net/license
+# *******************************************************************************
+
+require 'openstudio'
+require 'openstudio/measure/ShowRunnerOutput'
+require 'fileutils'
+
+require_relative '../measure.rb'
+require 'minitest/autorun'
+
+class UrbanoptCoreTestMeasure_Test < Minitest::Test
+  def test_UrbanoptCoreTestMeasure_BadInput
+    # create an instance of the measure
+    measure = UrbanoptCoreTestMeasure.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # make an empty model
+    model = OpenStudio::Model::Model.new
+
+    # get arguments and test that they are what we are expecting
+    arguments = measure.arguments(model)
+    assert_equal(2, arguments.size)
+    assert_equal('meter_name', arguments[0].name)
+    assert_equal('reporting_frequency', arguments[1].name)
+
+    # set argument values to bad values and run the measure
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+    meter_name = arguments[0].clone
+    assert(meter_name.setValue(''))
+    argument_map['meter_name'] = meter_name
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    assert(result.value.valueName == 'Fail')
+  end
+
+  def test_UrbanoptCoreTestMeasure_GoodInput
+    # create an instance of the measure
+    measure = UrbanoptCoreTestMeasure.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    # make an empty model
+    model = OpenStudio::Model::Model.new
+
+    # get arguments and test that they are what we are expecting
+    arguments = measure.arguments(model)
+
+    # set argument values to good values and run the measure on model with spaces
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+    meter_name = arguments[0].clone
+    assert(meter_name.setValue('JustATest'))
+    argument_map['meter_name'] = meter_name
+    reporting_frequency = arguments[1].clone
+    assert(reporting_frequency.setValue('hourly'))
+    argument_map['reporting_frequency'] = reporting_frequency
+    measure.run(model, runner, argument_map)
+    result = runner.result
+    show_output(result)
+    assert(result.value.valueName == 'Success')
+    assert(result.warnings.empty?)
+    assert(result.info.size == 1)
+
+    # attempt to add a second meter
+    # create an instance of the measure
+    measure = UrbanoptCoreTestMeasure.new
+
+    # create an instance of a runner
+    runner = OpenStudio::Measure::OSRunner.new(OpenStudio::WorkflowJSON.new)
+
+    measure.run(model, runner, argument_map)
+
+    result = runner.result
+    show_output(result)
+    assert(result.value.valueName == 'Success')
+    assert(result.warnings.size == 1)
+    assert(result.info.empty?)
+  end
+end
